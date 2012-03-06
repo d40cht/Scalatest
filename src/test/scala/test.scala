5ac6de20317c932b05ca6a324ff8411ef42b5733
Alex Wilson <alex_wilson@pobox.com>
2012-03-06 18:33:05 +0000

    Placeholder for manifests.

--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -311,6 +311,10 @@ class Test1 extends FunSuite
         }
     }
     
+    test( "Manifests" )
+    {
+    }
+    
     test( "Collection munging" )
     {
         val a = List(1,2,3,4,5,6,7,8,9,10,11,12,13,14)
